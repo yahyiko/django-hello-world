b75e66be0698bd9371de1daf4dd41ace436c3156
yahyiko <nico.yah@marquette.edu>
2023-04-30 14:16:22 -0500

    Update views.py

--- example/views.py
+++ example/views.py
@@ -8,7 +8,7 @@ def index(request):
     html = f'''
     <html>
         <body>
-            <h1>Hello from Vercel!</h1>
+            <h1>Bitches arent real fr fr</h1>
             <p>The current time is { now }.</p>
         </body>
     </html>
